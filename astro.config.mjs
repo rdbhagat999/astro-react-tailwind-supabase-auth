9d8a84610952f502676d7abfb15f028f81627a03
Ramandeep Bhagat <rdbhagat999@gmail.com>
2023-01-30 01:07:56 +0530

    adds vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -7,13 +7,14 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel(),
 });
