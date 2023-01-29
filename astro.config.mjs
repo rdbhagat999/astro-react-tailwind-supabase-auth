8e03fa548d456c0468428e1f94c28b7ba917f119
Ramandeep Bhagat <rdbhagat999@gmail.com>
2023-01-29 21:19:07 +0530

    starter-pack

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: 'standalone'
+  })
+});
